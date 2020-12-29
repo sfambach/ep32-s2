56be3fa977ca4e7ee7ac4f226527a974ee767a3b
Stefan Fambach <spam1@bobbelsche.de>
2020-12-29 18:42:51 +0100

    removed lib

--- boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/grabRef.cmake
+++ boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/grabRef.cmake
@@ -15,7 +15,7 @@
 
 set(HEAD_HASH)
 
-file(READ "C:/Devel/Git/esp32-s2/boards/LilyGoT-Display-S2/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
+file(READ "C:/Devel/Git/esp32-s2/boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
 set(GIT_DIR "C:/Devel/Git/esp32-s2/.git")
@@ -34,17 +34,17 @@ if(HEAD_CONTENTS MATCHES "ref")
 	# named branch
 	string(REPLACE "ref: " "" HEAD_REF "${HEAD_CONTENTS}")
 	if(EXISTS "${GIT_DIR}/${HEAD_REF}")
-		configure_file("${GIT_DIR}/${HEAD_REF}" "C:/Devel/Git/esp32-s2/boards/LilyGoT-Display-S2/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" COPYONLY)
+		configure_file("${GIT_DIR}/${HEAD_REF}" "C:/Devel/Git/esp32-s2/boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" COPYONLY)
 	elseif(EXISTS "${GIT_DIR}/logs/${HEAD_REF}")
-		configure_file("${GIT_DIR}/logs/${HEAD_REF}" "C:/Devel/Git/esp32-s2/boards/LilyGoT-Display-S2/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" COPYONLY)
+		configure_file("${GIT_DIR}/logs/${HEAD_REF}" "C:/Devel/Git/esp32-s2/boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" COPYONLY)
 		set(HEAD_HASH "${HEAD_REF}")
 	endif()
 else()
 	# detached HEAD
-	configure_file("${GIT_DIR}/HEAD" "C:/Devel/Git/esp32-s2/boards/LilyGoT-Display-S2/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" COPYONLY)
+	configure_file("${GIT_DIR}/HEAD" "C:/Devel/Git/esp32-s2/boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" COPYONLY)
 endif()
 
 if(NOT HEAD_HASH)
-	file(READ "C:/Devel/Git/esp32-s2/boards/LilyGoT-Display-S2/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" HEAD_HASH LIMIT 1024)
+	file(READ "C:/Devel/Git/esp32-s2/boards/LILYGO_EP32-S2_DISPLAY_V1_1/src/IDF_TTGO-Display-S2-DisplLib/build/CMakeFiles/git-data/head-ref" HEAD_HASH LIMIT 1024)
 	string(STRIP "${HEAD_HASH}" HEAD_HASH)
 endif()
